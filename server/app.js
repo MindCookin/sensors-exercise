82c74033d4461b56d6beb750ace870940437cd7d
Santiago Bernabé <santiago.bernabe@medianet.es>
2016-05-26 19:37:32 +0200

    Create api helper

--- server/app.js
+++ server/app.js
@@ -1,20 +1,17 @@
 "use strict";
 
 const DB = require('./db');
+const api_helper = require('./helper');
 
-const os = require('os');
 const path = require('path');
 const express = require('express');
 const multer = require('multer');
 const streamBuffers = require('stream-buffers');
 const bodyParser = require('body-parser');
-const csv = require('csv-parser');
-const split = require('split');
 
 const storage = multer.memoryStorage();
 const upload = multer({storage});
 const app = express();
-
 
 app.use(express.static('public'));
 
@@ -24,22 +21,7 @@ app.get('/', (req, res) => {
 
 app.post('/graph', bodyParser.urlencoded(), (req, res) => {
 
-  console.log("request: ", req.body);
-  let query = {
-    name: req.body.name,
-    timestamp: {
-      $gt: req.body.from && new Date(req.body.from).getTime() || 0,
-      $lt: req.body.to && new Date(req.body.to).getTime() || Number.MAX_SAFE_INTEGER,
-    }
-  };
-
-  if (req.body.signal) {
-    query.signal = {'$in': (typeof req.body.signal !== 'string' ) ? req.body.signal : [req.body.signal]};
-  }
-
-  console.log(query)
-
-  DB.get(query)
+  DB.get( api_helper.fetchQuery(req.body) )
     .then((results) => {
       console.log('received metrics: ', results);
       res.send(results);
@@ -48,34 +30,7 @@ app.post('/graph', bodyParser.urlencoded(), (req, res) => {
 
 app.post('/files', upload.array('myfile'), (req, res) => {
 
-  let data, values, query = [];
-  let signal, timestamp, value;
-
-  // TODO: this should be done asynchronously with streams
-  for (let file of req.files){
-
-    let name = file.originalname.split('-')[0];
-    let acquisitionTime = file.originalname.split('-')[1].replace('.csv', '');
-
-    file.buffer.toString()
-      .split(os.EOL)
-      .forEach((line, index) => {
-        if (index > 0) {
-          values = line.split(',');
-          signal = values[0];
-          timestamp = parseInt(values[1]);
-          value = parseInt(values[2]);
-
-          if (signal && timestamp && value) {
-            query.push({name, signal, timestamp, value})
-          }
-        }
-      });
-  }
-
-  console.log(query);
-
-  DB.insert(query)
+  DB.insert(api_helper.insertQuery(req.files))
     .then((results) => {
       console.log('saved to database');
       res.send(results);
